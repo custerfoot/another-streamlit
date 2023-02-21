4416646e044babd9075b9a3b13acf9459f0b5749
Kamil Bregula <kamil.bregula@snowflake.com>
2023-02-21 14:59:44 +0100

    fixup! fixup! Add ability to turn off anchors

--- lib/tests/streamlit/streamlit_test.py
+++ lib/tests/streamlit/streamlit_test.py
@@ -399,26 +399,21 @@ class StreamlitAPITest(DeltaGeneratorTestCase):
 
     def test_st_help(self):
         """Test st.help."""
-        st.help(st.header)
+        st.help(st.help)
 
         el = self.get_delta_from_queue().new_element
-        self.assertEqual(el.doc_string.name, "header")
+        self.assertEqual(el.doc_string.name, "help")
         self.assertEqual(el.doc_string.module, "streamlit")
         self.assertTrue(
-            el.doc_string.doc_string.startswith("Display text in header formatting.")
+            el.doc_string.doc_string.startswith(
+                "Display object's doc string, nicely formatted."
+            )
         )
         self.assertEqual(el.doc_string.type, "<class 'method'>")
-        if sys.version_info < (3, 9):
-            # Python < 3.9 represents the signature slightly differently
-            self.assertEqual(
-                el.doc_string.signature,
-                "(body: object, anchor: Union[str, NoneType] = None) -> 'DeltaGenerator'",
-            )
-        else:
-            self.assertEqual(
-                el.doc_string.signature,
-                "(body: object, anchor: Optional[str] = None) -> 'DeltaGenerator'",
-            )
+        self.assertEqual(
+            el.doc_string.signature,
+            "(obj: Any) -> 'DeltaGenerator'",
+        )
 
     def test_st_info(self):
         """Test st.info."""
