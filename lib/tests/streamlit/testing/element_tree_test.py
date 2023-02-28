b582c69270586f982f93f3cca524f71e4405b1fe
Kamil Bregula <kamil.bregula@snowflake.com>
2023-02-28 08:17:44 +0100

    Use constant in test

--- lib/tests/streamlit/testing/element_tree_test.py
+++ lib/tests/streamlit/testing/element_tree_test.py
@@ -16,6 +16,7 @@ from datetime import datetime, time
 
 import pytest
 
+from streamlit.elements.markdown import MARKDOWN_HORIZONTAL_RULE_EXPRESSION
 from streamlit.testing.script_interactions import InteractiveScriptTests
 
 
@@ -172,7 +173,7 @@ class MarkdownTest(InteractiveScriptTests):
 
         assert sr.get("divider")
         assert sr.get("divider")[0].type == "divider"
-        assert sr.get("divider")[0].value == "---"
+        assert sr.get("divider")[0].value == MARKDOWN_HORIZONTAL_RULE_EXPRESSION
 
     def test_markdown_elements_by_type(self):
         script = self.script_from_string(
